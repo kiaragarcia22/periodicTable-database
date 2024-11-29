a855770f737128788c195fceb6c8ea87c484297a
kiaragarcia22 burgos <kiaragarciaa88@gmail.com>
2024-11-29 23:19:55 +0000

    feat: Add skeleton

--- element.sh
+++ element.sh
@@ -1,1 +1,35 @@
-echo "Please provide an element as an argument." 
+#!/bin/bash
+PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
+
+if [[ -z $1 ]]
+then
+  echo "Please provide an element as an argument."
+else
+  
+  if [[ ! $1 =~ ^[0-9]+$ ]]
+  then 
+    
+    #ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE symbol = '$1' OR name = '$1'")
+    ELEMENT_NAME=$($PSQL "SELECT name FROM elements WHERE symbol = '$1' OR name = '$1'")
+    ELEMENT_SYMBOL=$($PSQL "SELECT symbol FROM elements WHERE symbol = '$1' OR name = '$1'")
+  else
+    
+    ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE atomic_number = $1")
+    #ELEMENT_NAME=$($PSQL "SELECT name FROM elements WHERE atomic_number = $1")
+    ELEMENT_SYMBOL=$($PSQL "SELECT symbol FROM elements WHERE atomic_number = $1")
+  fi
+
+  if [[ -z $ATOMIC_NUMBER ]]
+  then
+    #echo "I could not find that element in the database."
+  else
+    
+    ATOMIC_MASS=$($PSQL "SELECT atomic_mass FROM properties WHERE atomic_number = $ATOMIC_NUMBER")
+    MELTING_POINT=$($PSQL "SELECT melting_point_celsius FROM properties WHERE atomic_number = $ATOMIC_NUMBER")
+    BOILING_POINT=$($PSQL "SELECT boiling_point_celsius FROM properties WHERE atomic_number = $ATOMIC_NUMBER")
+    TYPE_ID=$($PSQL "SELECT type_id FROM properties WHERE atomic_number = $ATOMIC_NUMBER")
+    #TYPE=$($PSQL "SELECT type FROM types WHERE type_id = $TYPE_ID")
+
+    echo "The element with atomic number $(echo "$ATOMIC_NUMBER" | sed -r 's/^ *| *$//g') is $(echo "$ELEMENT_NAME" | sed -r 's/^ *| *$//g') ($(echo "$ELEMENT_SYMBOL" | sed -r 's/^ *| *$//g')). It's a $(echo "$TYPE" | sed -r 's/^ *| *$//g'), with a mass of $(echo "$ATOMIC_MASS" | sed -r 's/^ *| *$//g') amu. $(echo "$ELEMENT_NAME" | sed -r 's/^ *| *$//g') has a melting point of $(echo "$MELTING_POINT" | sed -r 's/^ *| *$//g') celsius and a boiling point of $(echo "$BOILING_POINT" | sed -r 's/^ *| *$//g') celsius."
+  fi
+fi
